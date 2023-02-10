ebeb740a87462d3c95fa1ea5ea22219897f775bb
“KazukiTakeuchi” <takeuchi1227813813@gmail.com>
2023-02-10 23:49:14 +0900

    必要のないセミコロンを削除

--- monorepo/packages/frontend/src/App.js
+++ monorepo/packages/frontend/src/App.js
@@ -1,33 +1,33 @@
-import { useState } from 'react';
+import { useState } from 'react'
 import User from './User'
 
 const getUsers = async () => {
-  const response = await fetch('/api/users');
-  const body = await response.json();
+  const response = await fetch('/api/users')
+  const body = await response.json()
   return body;
 }
 
 function App() {
-  const [users, setUsers] = useState(['a', 'b', 'c', 'd']);
-  const [inputText, setInputText] = useState('');
+  const [users, setUsers] = useState(['a', 'b', 'c', 'd'])
+  const [inputText, setInputText] = useState('')
 
   getUsers()
     .then((data) => console.log(data))
-    .catch((err) => console.log(err));
+    .catch((err) => console.log(err))
 
   const userList = users.map((user) => {
     return <User key={user} name={user} />
   })
 
   const handleSubmit = (event) => {
-    event.preventDefault();
-    const newUsers = [...users, inputText];
-    setUsers(newUsers);
-  };
+    event.preventDefault()
+    const newUsers = [...users, inputText]
+    setUsers(newUsers)
+  }
 
   const handleChange = (event) => {
-    setInputText(event.target.value);
-  };
+    setInputText(event.target.value)
+  }
 
   return (
     <div className="App">
@@ -37,7 +37,7 @@ function App() {
         <button type="submit">追加</button>
       </form>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
